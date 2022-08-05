d57a8311bda493d9a9e3c037e5e3b503ba0e081c
artyombuzuk16 <artyombuzuk16@gmail.com>
2022-08-05 20:38:40 +0300

    edit layout

---
+++ app/src/main/java/com/artyombuzuk/reddit/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.artyombuzuk.reddit.view
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import com.artyombuzuk.reddit.R
+import com.artyombuzuk.reddit.databinding.ActivityMainBinding
+import org.koin.core.component.KoinComponent
+
+class MainActivity : AppCompatActivity(), KoinComponent {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
