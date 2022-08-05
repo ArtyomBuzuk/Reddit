7b96fff735ce4aa9ec7a38a5397e98aeb57d0bc7
artyombuzuk16 <artyombuzuk16@gmail.com>
2022-08-05 12:02:08 +0300

    add Api

---
+++ app/src/main/java/com/artyombuzuk/reddit/data/RedditApi.kt
@@ -0,0 +1,13 @@
+package com.artyombuzuk.reddit.data
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RedditApi {
+    @GET("/top.json")
+    suspend fun getTopReddits(
+        @Query("after") after: String? = null,
+        @Query("limit") limit: Int = 10
+    ): Response<RemoteRedditPostList>
+}
